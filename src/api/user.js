581465dfd6abb0c1b44b24946235db2aa57ef6b2
xieqifeng <xieqifeng@xiaomi.com>
2020-04-15 10:52:28 +0800

    修改登出接口

--- src/api/user.js
+++ src/api/user.js
@@ -20,7 +20,7 @@ export function getInfo(token) {
 export function logout() {
   return request({
     // url: '/vue-admin-template/user/logout',
-    url: '/user/logOut',
+    url: '/user/logout',
     method: 'post'
   })
 }
