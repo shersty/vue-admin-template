cda0847e0e72b5dc85ae0a9d0c8fad95eb0be07e
xieqifeng <xieqifeng@xiaomi.com>
2020-04-15 15:50:06 +0800

    suit列表页改写

---
+++ src/views/suit/components/my-suit-list.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-collapse>
+      <el-collapse-item
+        v-for="suit in suitList"
+        :key="suit.id"
+        :title="suit.desc + '||' +suit.id"
+      >
+        <el-table
+          :data="testApiList"
+          style="width: 100%"
+        >
+          <el-table-column
+            v-for="{ prop, label } in colConfigs"
+            :key="prop"
+            :prop="prop"
+            :label="label"
+          />
+        </el-table>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SuitList',
+  props: {
+    suitList: {
+      type: Array,
+      default: () => ({})
+    }
+  },
+  data() {
+    this.colConfigs = [
+      { prop: 'desc', label: '描述' },
+      { prop: 'id', label: 'ID' }
+    ]
+    this.testApiList = [
+      { desc: '接口1', id: '1' },
+      { desc: '接口2', id: '2' }
+    ]
+    return {
+    }
+  }
+}
+</script>
--- src/views/suit/list.vue
+++ src/views/suit/list.vue
@@ -1,53 +1,19 @@
 <template>
-  <div>
-    <!-- <el-table
-      :data="suitList"
-      style="width: 100%"
-    >
-      <el-table-column
-        v-for="{ prop, label } in colConfigs"
-        :key="prop"
-        :prop="prop"
-        :label="label"
-      />
-    </el-table>
-    <el-divider /> -->
-    <el-collapse>
-      <el-collapse-item
-        v-for="suit in suitList"
-        :key="suit.id"
-        :title="suit.desc + '||' +suit.id"
-      >
-        <el-table
-          :data="testApiList"
-          style="width: 100%"
-        >
-          <el-table-column
-            v-for="{ prop, label } in colConfigs"
-            :key="prop"
-            :prop="prop"
-            :label="label"
-          />
-        </el-table>
-      </el-collapse-item>
-    </el-collapse>
-  </div>
+  <MySuiList :suit-list="suitList" />
 </template>
 
 <script>
 import { getSuitList } from '../../api/suit'
+import MySuiList from './components/my-suit-list'
 export default {
+  components: {
+    MySuiList
+  },
   data() {
     // 配置suit列表显示列
     this.colConfigs = [
       { prop: 'desc', label: '描述' },
       { prop: 'id', label: 'ID' }
-    ]
-    this.testApiList = [
-      { desc: 'api1',
-        id: 'api/test1' },
-      { desc: 'api2',
-        id: 'api/test2' }
     ]
     return {
       suitList: null
